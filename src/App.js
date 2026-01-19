cc4068ed38fdd58b4805c06cc33e28d5e2342587
ze-nien <bb0145786932@gmail.com>
2026-01-19 21:22:21 +0800

    dev d1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import Info from "./info";
+import Nav from "./Nav";
+
 function App() {
+  let fri = ["a", "b", "C"];
+  let fri1 = "a";
   return (
     <div>
       <h1>app.js practice</h1>
+      <Nav />
+      <Info name={fri1} />
+      <Info name={fri[1]} />
+      <Info name="c" />
     </div>
   );
 }
---
+++ src/info.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles/style.css";
+
+function info() {
+  return (
+    <div className="info">
+      <h1>info組件</h1>
+      {}
+    </div>
+  );
+}
+
+export default info;
