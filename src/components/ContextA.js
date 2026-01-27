87b7e31d45324038affa9052da42f01f675bc1d5
ze-nien <bb0145786932@gmail.com>
2026-01-27 22:04:04 +0800

    dev d5

---
+++ src/components/ContextA.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ContextB from "./ContextB";
+
+const ContextA = () => {
+  return (
+    <div>
+      <ContextB />
+    </div>
+  );
+};
+
+export default ContextA;
---
+++ src/components/ContextB.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ContextC from "./ContextC";
+const ContextB = () => {
+  return (
+    <div>
+      <ContextC />
+    </div>
+  );
+};
+
+export default ContextB;
